c549c9425f85848be25418a409d8d203a1b18c07
Sinon <malovu01@mail.ru>
2022-02-08 04:13:20 +0600

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+
+]
